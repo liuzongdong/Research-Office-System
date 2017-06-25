865a8b7f250f1f131f37d02916b82e830ceb51ab
Ethan LIU <ethanliuzongdong@gmail.com>
2017-06-25 23:06:14 +0800

    Change the list of the submenu

--- iv_project/index.php
+++ iv_project/index.php
@@ -54,10 +54,9 @@
 
     <div class="submenu">
     <ul>
-		<li><a href="/uic_project" >Category I - III</a></li>
+		<li><a href="/uic_project">Category I - III</a></li>
 		<li><a href="/iv_project" class="selected">Category IV</a></li>
-		<li><a href="">UIC Project Budget</a></li>
-		<li><a href="">Project Undertaking</a></li>
+		<li><a href="/project_undertaking">UIC Project Budget & Project Undertaking</a></li>
     </ul>
     </div>
 
